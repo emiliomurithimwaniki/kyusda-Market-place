08161d2cf68bd5803c3264d93a688536a9222516
Emilio Murithi Mwaniki <emiliomurithi4@gmail.com>
2026-04-01 22:07:47 +0300

    Admin Apps

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Follow, Profile, User
+
+
+admin.site.register(User)
+admin.site.register(Profile)
+admin.site.register(Follow)
